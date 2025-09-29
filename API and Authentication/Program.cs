f25a06691aebaa6c2be543bda7ac0fdb23df44f3
Ahmad-K-Hamdan <Ahmadkhamdan@outlook.com>
2025-09-29 21:42:51 +0300

    Implement JWT authentication; Added TokenRequest model; Implemented token generation API and sample API

--- API and Authentication/Program.cs
+++ API and Authentication/Program.cs
@@ -1,12 +1,82 @@
+using API_and_Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSingleton<JwtTokenGenerator>();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+        };
+    });
+
+builder.Services.AddSwaggerGen(swagger =>
+{
+    swagger.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "v1",
+        Version = "ASP.NET 9 Web API",
+        Description = "Exercise 7. Minimal APIs and JWT Authentication"
+    });
+
+    // JWT Authentication in Swagger
+    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+    });
+
+    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapPost("/tokens", (TokenRequest request, JwtTokenGenerator jwtGen) =>
+{
+    var token = jwtGen.GenerateToken(request.Username, request.Email);
+    return Results.Ok(new { token });
+}).AllowAnonymous();
+
+app.MapGet("/messages", () => "Hello there")
+    .RequireAuthorization();
 
 app.Run();
